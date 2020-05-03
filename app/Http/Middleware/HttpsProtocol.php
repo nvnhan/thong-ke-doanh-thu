0022d72e905845ee428e4d3b0243603f66afce3a
NV Nhận <nv.bigbang@gmail.com>
2020-05-03 22:42:53 +0700

    Update redirect SSL

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -4,8 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\App;
+use Illuminate\Foundation\Http\Middleware\TrimStrings as Middleware;
 
-class HttpsProtocol
+class HttpsProtocol extends Middleware
 {
 
     public function handle($request, Closure $next)

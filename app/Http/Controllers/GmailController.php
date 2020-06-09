eab7dec8cffa14e3cbcd5bab59dbd9f3cfe4cf80
NV Nhận <nv.bigbang@gmail.com>
2020-06-09 19:36:09 +0700

    Update gmail service

--- app/Http/Controllers/GmailController.php
+++ app/Http/Controllers/GmailController.php
@@ -15,13 +15,15 @@ class GmailController extends BaseController
     public function login(Request $request)
     {
         $credential = $request->only(['username', 'password']);
-        if (Auth::attempt($credential, true)) {
+        if (Auth::attempt($credential)) {
             $user_id = $request->user()->id;        // ~ auth()->user()->id
             $disk = Storage::disk('local');
             $file = "gmail/tokens/gmail-json-$user_id.json";
             // Delete old token file
             if ($disk->exists($file)) $disk->delete($file);
 
+            // Set auth cookie to browser
+            // Then redirect to gmail login
             return view('redirect-to-gmail');
         } else return 'Login failed';
     }

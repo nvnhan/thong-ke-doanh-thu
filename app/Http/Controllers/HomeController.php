b4a8e6940952e1fa087ede4de142e242c811b17a
NV Nhận <nv.bigbang@gmail.com>
2021-01-18 11:18:59 +0700

    Fix Mat Ve HTML

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,7 +38,7 @@ class HomeController extends BaseController
         $user = User::find($u);
         $objs = explode(',', $ids);
         if (\is_array($objs)) {
-            $datves = DatVe::withoutGlobalScope(OfUserScope::class)->where('username', $user->username)->whereIn('id', $objs)->get();
+            $datves = DatVe::withoutGlobalScope(OfUserScope::class)->whereIn('id', $objs)->get();
             $dv = $datves->where('hang_bay', $datves[0]->hang_bay);
             if ($dv[0]->hang_bay === 'VN' || $dv[0]->hang_bay === 'BB')
                 $dv = $dv->where('ma_giu_cho', $dv[0]->ma_giu_cho);
@@ -51,7 +51,11 @@ class HomeController extends BaseController
             $sb_di1 = SanBay::where('ma_san_bay', $datve->sb_di1)->first()->ten_san_bay ?? "";
             $sb_ve = SanBay::where('ma_san_bay', $datve->sb_ve)->first()->ten_san_bay ?? "";
             $sb_ve1 = SanBay::where('ma_san_bay', $datve->sb_ve1)->first()->ten_san_bay ?? "";
-            $datve = $datve->toArray();
+            $datve = $datve->setHidden([
+                'ma_khach_hang', 'noi_mua',
+                'da_thanh_toan', 'chua_thanh_toan',
+                'loai_hanh_ly'
+            ])->toArray();
 
             return view('mat-ve.index', compact('user', 'datve', 'sb_di', 'sb_di1', 'sb_ve', 'sb_ve1', 'dv'));
         }
